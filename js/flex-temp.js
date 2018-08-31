32f437f6da34e778b5868633e3449d5b29f294a4
Veselov1985 <xoxorrrr>
2018-08-31 17:17:16 +0300

    add parsing block

--- js/flex-temp.js
+++ js/flex-temp.js
@@ -1,16 +1,6 @@
 var ft = {};
 
-ft.elements = {
-    tooltip: '',
-};
-
-
-ft.validate = {
-
-};
-
-
-
+ft.elements = {};
 
 ft.elInit = function() {
     ft.elements.ft_input = $('#ft_input');
@@ -18,21 +8,179 @@ ft.elInit = function() {
     ft.elements.ft_btn = $('#ft_btn');
 };
 
-
 ft.actionsInit = function() {
     ft.elements.ft_btn.on('click', function() {
-
+        ft.validate.init();
     });
 };
 
+ft.validate = {
+    init: function() {
+        var UsePage = ft.helpfunc.input.getValue().trim().toLowerCase();
+        ft.validate.shakeValidate(UsePage);
+    },
+    shakeValidate: function(string) {
+        var p;
+        var PageFrom = ft.helpfunc.select.getOptionVal();
+        if (ft.validate.digitTest(string)) {
+            p = ft.parsing.digitPars(string, PageFrom);
+
+        } else if (ft.validate.nTest(string)) {
+            p = ft.parsing.nPars(string, PageFrom);
+
+        } else if (ft.validate.starTest(string)) {
+            p = ft.parsing.starPars(string, PageFrom);
+
+        } else {
+            temp.helpfunc.modalInfo(['Field Copy To', 'Incorrect Enter']);
+        }
+    },
+    digitTest: function(str) {
+        var pagesInDocument = temp.DataWorkspace.images.length;
+        var test = true;
+        var res = str.split(',');
+        res.forEach(function(val) {
+            if (!$.isNumeric(val) || +val > pagesInDocument) test = false;
+        });
+        return test;
+    },
+    nTest: function(string) {
+        var res;
+        var test = false;
+        var pagesInDocument = temp.DataWorkspace.images.length;
+        var matchN = string.match(/n/ig);
+        if (matchN == null || matchN.length > 1) {
+            test = false;
+        } else {
+            res = string.split('');
+            if (res.length == 1 && res[0] == 'n') {
+                test = true;
+            }
+            if (res.length == 3 && res[0] == 'n' && res[1] == '-' && $.isNumeric(res[2]) && +res[2] < pagesInDocument) {
+                test = true;
+            }
+        }
+        return test;
+    },
+    starTest: function(string) {
+        var res, test;
+        var pagesInDocument = temp.DataWorkspace.images.length;
+        test = false;
+        var matchN = string.match(/\*/ig);
+        if (matchN == null || matchN.length > 1) {
+            test = false;
+        } else {
+            res = string.split('');
+            if (res[0] == '*' && res[1] == "-" && $.isNumeric(res[2]) && pagesInDocument - (+res[2]) > 0) {
+                test = true;
+            }
+            var fromUpToAll = res.join('').split('-');
+            if ($.isNumeric(fromUpToAll[0]) &&
+                +fromUpToAll[0] < pagesInDocument &&
+                fromUpToAll[1] == "*" &&
+                $.isNumeric(fromUpToAll[2]) &&
+                +fromUpToAll[2] < pagesInDocument &&
+                (pagesInDocument - +fromUpToAll[0] - +fromUpToAll[2]) > 0
+            ) {
+                test = true;
+            } else if (fromUpToAll.length == 1 && fromUpToAll[0] == "*") {
+                test = true;
+            }
+        }
+        return test;
+    },
+};
+
+ft.parsing = {
+    init: function() {
+        var PageFrom = +ft.helpfunc.select.getOptionVal();
+    },
+    digitPars: function(str, cf) { // cf => value selected option
+        return str.split(',').filter(function(val, i) {
+            return +val != cf;
+        });
+    },
+    nPars: function(str, cf) {
+        var p;
+        var pagesInDocument = temp.DataWorkspace.images.length;
+        var res = str.split("");
+        if (res.length == 1) {
+            p = [pagesInDocument].filter(function(val) { return pagesInDocument != val; });
+        } else {
+            p = [pagesInDocument - (+res[2])].filter(function(val) { return pagesInDocument != val; });
+        }
+        return p;
+    },
+    starPars: function(str, cf) {
+        var p, res, arr;
+        p = [];
+        arr = [];
+        var pagesInDocument = temp.DataWorkspace.images.length;
+        res = str.split('-');
+        if (res.length == 1 && res[0] == "*") {
+            arr.length = pagesInDocument;
+            arr.forEach(function(val, i) {
+                p.push(i + 1);
+            });
+        } else if (res.length == 2) {
+            if (res[0] == "*") { //*-1
+                arr.length = pagesInDocument - (+res[1]);
+                arr.forEach(function(val, i) {
+                    p.push(i + 1);
+                });
+
+            } else if (res[1] == '*') { //2 -*
+                arr.length = pagesInDocument;
+                arr.forEach(function(val, i) {
+                    if (val >= +res[0]) {
+                        p.push(i + 1);
+                    }
+                });
+            }
+        } else if (res.length == 3) { // 2-*-1
+            arr.length = pagesInDocument - (+res[2]);
+            arr.forEach(function(val, i) {
+                if (val >= +res[0]) {
+                    p.push(i + 1);
+                }
+            });
+        }
+        return p.filter(function(val) {
+            return val != cf;
+        });
+    },
+};
 
 
 ft.handlers = {
 
 };
 
-ft.tooltipInit = function() {
+ft.helpfunc = {
+    input: {
+        getValue: function() {
+            return ft.elements.ft_input.val();
+        },
+    },
+    select: {
+        renderSelect: function(pages) {
+            ft.helpfunc.select.cleanSelect();
+            pages.forEach(function(element, i) {
+                i += 1;
+                ft.elements.ft_select.append('<option value="' + i + '">' + i + '</option>');
+            });
+        },
+        cleanSelect: function() {
+            ft.elements.ft_select.empty();
+        },
+        getOptionVal: function() {
+            return ft.elements.ft_select.find('option:selected').val();
+        },
 
+    },
+};
+
+ft.tooltipInit = function() {
     var myTemplate = document.createElement('div');
     myTemplate.innerHTML = '<div style="width: 18rem;">' +
         '<div class="row">' +
@@ -61,7 +209,6 @@ ft.tooltipInit = function() {
         placement: 'right',
         html: myTemplate
     });
-
 };
 
 ft.init = function() {
@@ -69,6 +216,5 @@ ft.init = function() {
     ft.tooltipInit();
     ft.actionsInit();
 };
-
 
 ft.init();
